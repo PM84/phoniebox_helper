fc7053393fb72e9d08e1d738830811feb54ecb25
Peter Mayer <admin@pemasoft.de>
2021-12-17 00:46:26 +0100

    Make it ready for python3

--- drehregler.py
+++ drehregler.py
@@ -31,58 +31,58 @@ swLastState = GPIO.input(sw)
 
 counter = int(os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read())
 if counter == 0 : counter = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getbootvolume").read()
- 
+
 #define functions which will be triggered on pin state changes
 def clkClicked(channel):
         global counter
         global step
- 
+
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
- 
+
         if clkState == 0 and dtState == 1:
                 counter = int(os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read())
                 counter = counter + step
                 if counter > 100: counter = 100
-                os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(counter))
-                print ("Counter ", counter)
- 
+                os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(counter))
+#                print ("Counter ", counter)
+
 def dtClicked(channel):
         global counter
         global step
- 
+
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
-         
+
         if clkState == 1 and dtState == 0:
                 counter = int(os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read())
                 counter = counter - step
                 if counter < 0: counter = 0
-                os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(counter))
-                print ("Counter ", counter)
- 
+                os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(counter))
+#                print ("Counter ", counter)
+
 def swClicked(channel):
         global paused
         paused = not paused
-        if paused == True : 
-              os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=mute")
+        if paused == True :
+              os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=mute")
 #              os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=playerpause")
-        if paused == False : 
+        if paused == False :
               bootvol = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getbootvolume").read()
-              os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(bootvol))
+              os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(bootvol))
 #              os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=playerplay")
-        print ("Paused ", paused)
+#        print ("Paused ", paused)
 
-print ("Initial clk:", clkLastState)
-print ("Initial dt:", dtLastState)
-print ("Initial sw:", swLastState)
-print ("=========================================")
- 
+#print ("Initial clk:", clkLastState)
+#print ("Initial dt:", dtLastState)
+#print ("Initial sw:", swLastState)
+#print ("=========================================")
+
 #set up the interrupts
 GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=300)
 GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=300)
 GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=300)
- 
-input("Start monitoring input")
- 
+
+input()
+
 GPIO.cleanup()
