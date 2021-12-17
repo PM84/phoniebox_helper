384a8e5acfa646771afc96d6e5b20d5995f84483
Peter Mayer <peter.mayer@isb.bayern.de>
2021-12-17 12:07:12 +0100

    Adding Phoniebox methods

--- rotary_control.py
+++ rotary_control.py
@@ -10,6 +10,22 @@ devices = {dev.fd: dev for dev in devices}
 value = 1
 print("Value: {0}".format(value))
 
+
+def readVolume():
+    value = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read()
+    return int(value)
+def getBootVolume():
+    bootvol = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getbootvolume").read()
+    return int(bootvol)
+def getVolumeStep():
+    volStep = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolstep").read()
+    return int(volStep)
+def MuteAudio():
+    if readVolume() > 1:
+        os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=mute")
+    else:
+        os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(getBootVolume()))
+
 done = False
 while not done:
     r, w, x = select.select(devices, [], [])
@@ -17,9 +33,9 @@ while not done:
         for event in devices[fd].read():
             event = evdev.util.categorize(event)
             if isinstance(event, evdev.events.RelEvent):
-                value = value + event.event.value
+                value = readVolume() + getVolumeStep()
                 print("Value: {0}".format(value))
             elif isinstance(event, evdev.events.KeyEvent):
                 if event.keycode == "KEY_ENTER" and event.keystate == event.key_up:
-                    done = True
-                    break
+                    MuteAudio()
+                    print("Mute-Button gedrückt")
